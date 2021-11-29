024a3913d6c7d8a5aab3c1ad4ca3ad02e90a4403
Cassidy Linhares <cassidy.linhares@ontariotechu.net>
2021-11-28 20:48:56 -0500

    fixed host

--- capstoneApp/capstoneApp/settings.py
+++ capstoneApp/capstoneApp/settings.py
@@ -20,11 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY='d*o91_n&%lkcyh-xryk#h^5_^hkz81vgd)&o8p#mmg2@o=pw)3'
+SECRET_KEY='d*o91_n&%lkcyh-xryk#h^5_^hkz81vgd)&o8p#mmg2@o=pw)3'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'ecopalapi.com']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'ecopalapi.herokuapp.com']
 
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000'
